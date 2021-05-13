714731e601ed1a5031d6a9d0e08fd7e70b1f265b
Will Franklin <will@willpf.co.uk>
2021-05-13 12:12:03 +0100

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,26 @@
+export default {
+  css: [
+  ],
+  head: {
+    title: 'Beabee',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'my website description'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  modules: ['@nuxt/http'],
+  http: {serverTimeout: 5000},
+  serverMiddleware: [],
+  target: 'server',
+  build: { // https://github.com/nuxt/nuxt.js/issues/9224#issuecomment-830577523
+    babel: {
+      plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
+    },
+  },
+}
