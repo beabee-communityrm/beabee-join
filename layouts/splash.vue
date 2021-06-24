38b79b4002746afcf7a0e997f4dbc911c1e9de55
Will Franklin <will@willpf.co.uk>
2021-06-24 15:47:20 +0100

    Add splash layout

---
+++ layouts/splash.vue
@@ -0,0 +1,21 @@
+<style lang="scss">
+  @import "~bulma/sass/utilities/all";
+  .splash-wrapper {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    background-color: var(--c-grey-lighter);
+
+    padding: 1rem;
+    @include tablet {
+      padding: 2rem;
+    }
+  }
+</style>
+<template>
+  <div class="splash-wrapper">
+    <div class="container">
+      <Nuxt />
+    </div>
+  </div>
+</template>
