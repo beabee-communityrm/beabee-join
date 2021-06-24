783c2a4c675a6ca51e92651c94499c45aad399ac
Will Franklin <will@willpf.co.uk>
2021-06-24 15:39:48 +0100

    Error box styling

--- components/ErrorBox.vue
+++ components/ErrorBox.vue
@@ -1,13 +1,29 @@
-<style>
+<style lang="scss">
+  @import "~bulma/sass/utilities/all";
+
   .error-box {
     max-width: 30rem;
     margin: 0 auto;
+    background: url(~/assets/img/error.png) no-repeat;
+    background-position: 90% 30%;
+    padding: 1.5rem;
+
+    @include tablet {
+      padding: 3rem;
+    }
+  }
+
+  .error-box-title {
+    font-size: 2rem;
+    @include tablet {
+      font-size: 3rem;
+    }
   }
 </style>
 
 <template>
-  <div class="error-box box px-5 has-background-white">
-    <h1 class="title is-size-1">{{ title }}</h1> 
+  <div class="error-box box has-background-white">
+    <h1 class="title error-box-title mb-5">{{ title }}</h1>
     <slot></slot>
   </div>
 </template>
--- pages/join/duplicate-email.vue
+++ pages/join/duplicate-email.vue
@@ -1,11 +1,19 @@
 <template>
   <ErrorBox title="Duplicate email address">
-    <p class="is-size-5 mb-4">
-      This email address is already registered.
+    <p class="is-size-5 mb-5">
+      This email address is already registered, you can <a
+      href="/login">login</a> to proceeed.
     </p> 
     <p>
-      <a href="/login" class="button is-primary">Login</a>
-      <NuxtLink to="/join" class="button is-primary">Back to join</NuxtLink>
+      <NuxtLink to="/join" class="button is-medium is-primary is-fullwidth">
+        Back to join page
+      </NuxtLink>
     </p>
   </ErrorBox>
 </template>
+
+<script>
+  export default {
+    layout: 'splash'
+  };
+</script>
