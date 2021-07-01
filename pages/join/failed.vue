606b7595eebb70da44a837d6a75aaf04a54cd448
Will Franklin <will@willpf.co.uk>
2021-07-01 16:18:30 +0100

    Add generic join failed page

---
+++ pages/join/failed.vue
@@ -0,0 +1,19 @@
+<template>
+  <NoticeBox title="Something went wrong">
+    <p class="mb-5">
+      There was a problem setting up your membership, our team has been
+      notified. Please contact support if the problem persists.
+    </p> 
+    <p>
+      <NuxtLink to="/join" class="button is-medium is-primary is-fullwidth">
+        Back to join page
+      </NuxtLink>
+    </p>
+  </NoticeBox>
+</template>
+
+<script>
+  export default {
+    layout: 'splash'
+  };
+</script>
