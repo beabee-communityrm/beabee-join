07f495b6fe0db22b3a4820430e936050e420c857
Will Franklin <will@willpf.co.uk>
2021-07-01 11:33:07 +0100

    Use new slots

--- pages/join/setup.vue
+++ pages/join/setup.vue
@@ -5,33 +5,28 @@
     </JoinHeader>
     <p class="block">Please confirm your details</p>
     <Form method="POST" :checkForm="checkForm">
-      <section class="block">
-        <fieldset>
-          <Input name="email" label="Email" v-model="email" required />
-          <Input name="firstname" label="First name" v-model="firstname" required />
-          <Input name="lastname" label="Last name" v-model="lastname" required />
-          <Input name="addressLine1" label="Address line 1" v-model="address.line1" />
-          <Input name="addressLine2" label="Address line 2" v-model="address.line2" />
-          <div class="columns">
-            <div class="column">
-              <Input name="city" label="City/town" v-model="address.city" />
+      <template v-slot:inputs>
+        <section class="block">
+          <fieldset>
+            <Input name="email" label="Email" v-model="email" required />
+            <Input name="firstname" label="First name" v-model="firstname" required />
+            <Input name="lastname" label="Last name" v-model="lastname" required />
+            <Input name="addressLine1" label="Address line 1" v-model="address.line1" />
+            <Input name="addressLine2" label="Address line 2" v-model="address.line2" />
+            <div class="columns">
+              <div class="column">
+                <Input name="city" label="City/town" v-model="address.city" />
+              </div>
+              <div class="column">
+                <Input name="postcode" label="Postcode" v-model="address.postcode" />
+              </div>
             </div>
-            <div class="column">
-              <Input name="postcode" label="Postcode" v-model="address.postcode" />
-            </div>
-          </div>
-        </fieldset>
-      </section>
-      <section class="block">
-        <p>
-          <button
-            class="button wrap-text is-fullwidth is-primary"
-            :class="{'is-loading': isSubmitting}" :disabled="hasMounted && !canSubmit"
-          >
-            <span>Contribute <span class="hidden-nojs">{{ submitText }}</span></span>
-          </button>
-        </p>
-      </section>
+          </fieldset>
+        </section>
+      </template>
+      <template v-slot:submit>
+        Save
+      </template>
     </form>
   </div>
 </template>
