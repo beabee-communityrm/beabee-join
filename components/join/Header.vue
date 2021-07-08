b276c52e93230de091c8a3ce518c8fd546613469
Will Franklin <will@willpf.co.uk>
2021-07-08 15:46:27 +0100

    Remove margin from title

--- components/join/Header.vue
+++ components/join/Header.vue
@@ -3,7 +3,7 @@
     <div class="is-flex is-align-items-center mb-4">
       <img class="join-header-logo" width="60" src="~assets/img/newspaper-logo.svg" :alt="name">
       <div class="ml-4">
-        <h1 class="title is-4 mb-2">{{ title }}</h1>
+        <h1 class="title is-4 mb-0">{{ title }}</h1>
         <p class="mt-2" v-if="subtitle">{{ subtitle }}</p>
       </div>
     </div>
