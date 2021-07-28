a184873a573cf021baeb506d7e97150a61927a9b
Will Franklin <will@willpf.co.uk>
2021-07-28 14:19:44 +0100

    Use PNG for now as not all logos can be SVG

--- components/join/Header.vue
+++ components/join/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="block">
     <div class="is-flex is-align-items-center mb-4">
-      <img class="join-header-logo" width="60" src="~assets/client/img/logo.svg" :alt="name">
+      <img class="join-header-logo" width="60" src="~assets/client/img/logo.png" :alt="name">
       <div class="ml-4">
         <h1 class="title is-4 mb-0">{{ title }}</h1>
         <p class="mt-2" v-if="subtitle">{{ subtitle }}</p>
